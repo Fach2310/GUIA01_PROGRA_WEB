d2ccbccfaf514a3bec94d4ac1433d03b34791e9e
L33116 <L33116@lab.aca>
2025-01-16 11:50:43 -0500

    Ejercicio02

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import { calcularPromedio, filtrarNumerosMayores } from "./ejercicio1"
+import { Punto, distanciaEntrePuntos, moverPunto } from "./ejercicio2";
 const main = () =>{
+    //Pregunta1
+
     const numeros:number[]=[5,8,3,10,2,7];
     //Filtrar
     console.log(filtrarNumerosMayores(numeros,5))
     //Promedio
     console.log(calcularPromedio(numeros))
+
+    //Pregunta2
+    const p1:Punto=[1,2];
+    const p2:Punto=[4,6];
+    console.log(distanciaEntrePuntos(p1,p2))
+    console.log(moverPunto(p1,p2))
+
 }
 main()
